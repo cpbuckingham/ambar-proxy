2eed407274e9f37504ca66849b00d81acec2224d
Igor.S <fpd4444@gmail.com>
2017-05-02 15:51:20 +0300

    nginx config fixed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 echo "server {" >> /etc/nginx/conf.d/default.conf
 echo "  listen ${FE_EXT_PORT};" >> /etc/nginx/conf.d/default.conf
 echo "  client_max_body_size 1024m;" >> /etc/nginx/conf.d/default.conf
-echo "  location /api {" >> /etc/nginx/conf.d/default.conf
-echo "    proxy_pass http://webapi:8080/api;" >> /etc/nginx/conf.d/default.conf
+echo "  location /api/ {" >> /etc/nginx/conf.d/default.conf
+echo "    proxy_pass http://webapi:8080/api/;" >> /etc/nginx/conf.d/default.conf
 echo "  }" >> /etc/nginx/conf.d/default.conf
 echo "  location / {" >> /etc/nginx/conf.d/default.conf
 echo "    proxy_pass http://frontend:80;" >> /etc/nginx/conf.d/default.conf
@@ -17,8 +17,8 @@ then
     echo "server {" >> /etc/nginx/conf.d/default.conf
     echo "  listen ${API_EXT_PORT};" >> /etc/nginx/conf.d/default.conf
     echo "  client_max_body_size 1024m;" >> /etc/nginx/conf.d/default.conf
-    echo "  location /api {" >> /etc/nginx/conf.d/default.conf
-    echo "    proxy_pass http://webapi:8080/api;" >> /etc/nginx/conf.d/default.conf
+    echo "  location /api/ {" >> /etc/nginx/conf.d/default.conf
+    echo "    proxy_pass http://webapi:8080/api/;" >> /etc/nginx/conf.d/default.conf
     echo "  }" >> /etc/nginx/conf.d/default.conf
     echo "}" >> /etc/nginx/conf.d/default.conf
 fi
